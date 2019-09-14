a7946a27bd2b2ce20a85d153a7a3dbd55706320f
Dean Diakov <deandiakov@Chriss-MacBook-Pro.local>
2019-09-14 15:52:06 -0400

    aha

--- run.py
+++ run.py
@@ -11,63 +11,25 @@
 
 import time
 import cv2
+import requests
+import json
+from ibm_watson import VisualRecognitionV3
 
 # Import helper functions
-import tutorial_helpers as helpers
+#import tutorial_helpers as helpers
 
 # Import the Python wrapper for the ELL model
-import model
+#import model
+
+visual_recognition = VisualRecognitionV3(
+    '2019-02-11',
+    iam_apikey='dMbAv-vbgW7a6v8CyEGxtO3YUuajWcdh_QpevaHv5w6x')
+
+with open('./test.jpg', 'rb') as images_file:
+    classes = visual_recognition.classify(
+        images_file,
+        threshold='0.6',
+        owners=["me"]).get_result()
+    print(json.dumps(classes, indent=2))
 
 
-def get_image_from_camera(camera):
-    """Read an image from the camera"""
-    if camera:
-        ret, frame = camera.read()
-        if not ret:
-            raise Exception("your capture device is not returning images")
-        return frame
-    return None
-
-
-def main():
-    # Open the video camera. To use a different camera, change the camera
-    # index.
-    camera = cv2.VideoCapture(0)
-
-    # Read the category names
-    with open("categories.txt", "r") as categories_file:
-        categories = categories_file.read().splitlines()
-
-    while (cv2.waitKey(1) & 0xFF) == 0xFF:
-        # Get an image from the camera.
-        image = get_image_from_camera(camera)
-
-        # Prepare an image for processing
-        # - Resize and center-crop to the required width and height while
-        #   preserving aspect ratio.
-        # - OpenCV gives the image in BGR order. If needed, re-order the
-        #   channels to RGB.
-        # - Convert the OpenCV result to a std::vector<float>
-        input_data = helpers.prepare_image_for_model(
-            image, input_shape.columns, input_shape.rows, preprocessing_metadata=preprocessing_metadata)
-
-        # Wrap the resulting numpy array in a FloatVector
-        input_data = model.FloatVector(input_data)
-
-        # Send the image to the compiled model and get the predictions vector
-        # with scores, measure how long it takes
-        start = time.time()
-        predictions = model_wrapper.Predict(input_data)
-        end = time.time()
-
-        # Get the value of the top 5 predictions
-        top_5 = helpers.get_top_n(predictions, 5)
-
-        # Display the image
-        cv2.imshow("ELL model", image)
-
-    print("Mean prediction time: {:.0f}ms/frame".format(
-        mean_time_to_predict * 1000))
-
-if __name__ == "__main__":
-    main()
