c9cf160dab2d8009f320f5b968a19b3d501e89ed
Dean Diakov <deandiakov@Chriss-MBP.fios-router.home>
2019-09-14 19:01:49 -0400

    api works ahhh

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ import time
 import cv2
 import requests
 import json
-from ibm_watson import VisualRecognitionV3
+
 
 # Import helper functions
 #import tutorial_helpers as helpers
@@ -21,15 +21,37 @@ from ibm_watson import VisualRecognitionV3
 # Import the Python wrapper for the ELL model
 #import model
 
-visual_recognition = VisualRecognitionV3(
-    '2019-02-11',
-    iam_apikey='dMbAv-vbgW7a6v8CyEGxtO3YUuajWcdh_QpevaHv5w6x')
 
-with open('./test.jpg', 'rb') as images_file:
-    classes = visual_recognition.classify(
-        images_file,
-        threshold='0.6',
-        owners=["me"]).get_result()
-    print(json.dumps(classes, indent=2))
+
+#res = requests.get('https://gateway.watsonplatform.net/visual-recognition/api', auth=('apikey', 'iYZxsGs7z0-FxsgZmsaBqhkcf4NdMbM15WmwayaIAOr7'))
+
+
+params = (
+    ('version', '2019-02-11'),
+)
+
+files = {
+    'features': (None, 'objects'),
+    'collection_ids': (None, 'bed5f8bc-ecb9-4397-a256-278f52812823'),
+    'images_file': ('dice.png', open('test.jpg', 'rb')),
+}
+
+response = requests.post('https://gateway.watsonplatform.net/visual-recognition/api/v4/analyze', params=params, files=files, auth=('apikey', 'iYZxsGs7z0-FxsgZmsaBqhkcf4NdMbM15WmwayaIAOr7'))
+
+output = response.json()
+objectsList = output['images'][0]['objects']['collections'][0]['objects']
+
+cleanedList = []
+for obj in objectsList:
+    threshold = obj['score']
+    x1 = obj['left']
+    y1 = obj['top']
+    x2 = x1 + obj['width']
+    y2 = y1 + obj['height']
+
+
+
+
+
 
 
